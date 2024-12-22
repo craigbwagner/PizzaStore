22b819f22efb1edfecd8407d6ecc6428198f1e69
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-12-22 14:20:37 -0500

    add get route for single pizza by id

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
 app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
 {
   await db.Pizzas.AddAsync(pizza);
--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de8504827a660955d83aadad94ad053406e8f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d71bc28d6865e0e52bb849570821d4ce593ff8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
